8f0be359127689f3394fdd645571e5e8941da6a5
Bruno_Marinho <bcaldasmarinhojklb@gmail.com>
2022-05-10 20:43:55 -0300

    feat: hook nav

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,27 @@
 import { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+
+
 import Navbar from '../components/Navbar'
 
 import '../styles/global.scss'
 
-function MyApp({ Component, pageProps }:AppProps) {
-  return(
+function MyApp({ Component, pageProps }: AppProps) {
+
+  const router = useRouter()
+
+  const [statusNavBar, setStatusNavbar] = useState(true)
+
+  useEffect(() => {
+    if (router.asPath == "/") {
+      setStatusNavbar(!statusNavBar)
+    }
+  }, [])
+
+  return (
     <>
-      <Navbar/>
+      {statusNavBar ? <Navbar /> : null}
       <Component {...pageProps} />
     </>
   )
