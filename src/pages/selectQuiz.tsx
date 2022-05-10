0329bf28c64331d971236ab7e7df17f064487d1f
Bruno_Marinho <bcaldasmarinhojklb@gmail.com>
2022-05-10 20:47:38 -0300

    :feat component nivelNumber

---
+++ src/pages/selectQuiz.tsx
@@ -0,0 +1,19 @@
+import styles from './styles.module.scss'
+
+import NivelNumber from "../components/NivelNumber";
+
+export default function selectQuiz(){
+    return(
+        <div className={styles.content}>
+            <NivelNumber
+                stylesColor={'var(--ceano)'}
+                NivelNumber={1}
+            />
+
+            <NivelNumber
+                stylesColor={'var(--orange)'}
+                NivelNumber={2}
+            />
+        </div>
+    )
+}
