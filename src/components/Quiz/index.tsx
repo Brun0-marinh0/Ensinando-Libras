9bf7af1b24969ee12e6631f31a6e5801420778e2
Bruno_Marinho <bcaldasmarinhojklb@gmail.com>
2022-05-17 19:00:13 -0300

    integração

--- src/components/Quiz/index.tsx
+++ src/components/Quiz/index.tsx
@@ -1,7 +1,43 @@
+import { useEffect, useState } from 'react'
+import { SelectQuestion } from '../../controllers/questions/select-question'
 import style from './styles.module.scss'
 
-export default function Quiz(){
-    return(
+
+interface QuestionType {
+    data: {
+        answer: string,
+        id: number,
+        created_at: Date,
+        question: string,
+        status: boolean,
+        type: {
+            description: string
+        },
+        options: {
+            image_a: string,
+            image_b: string,
+            image_c: string,
+            image_d: string,
+            option_a: string,
+            option_b: string,
+            option_c: string,
+            option_d: string
+        }
+    }
+}
+
+export default function Quiz() {
+
+    const [question, setQuestion] = useState<QuestionType>()
+
+    useEffect(() => {
+        SelectQuestion().then(res => setQuestion(res.data)).catch(e => console.log(e))
+
+        console.log( "to auqi", question)
+
+    }, [])
+
+    return (
         <div className={style.frame}>
 
             <div className={style.nivel}>
@@ -16,13 +52,10 @@ export default function Quiz(){
             </div>
 
             <div className={style.quest}>
-                <h1>1-pergunta</h1>
+                <h1>1-{ question.data.id }</h1>
             </div>
             <div>
                 <div className={style.options}>
-                    <div></div>
-                    <div></div>
-                    <div></div>
                     <div></div>
                 </div>
             </div>
